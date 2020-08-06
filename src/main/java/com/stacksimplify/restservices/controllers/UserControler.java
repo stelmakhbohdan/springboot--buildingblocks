e659de96b5c4d341ab6a0393ab0dd84983b86822
Bohdan Stelmakh <bohdan.stelmakh@softiero.com>
2020-08-06 12:06:16 +0300

    ResponseStatusException First Commit

--- src/main/java/com/stacksimplify/restservices/controllers/UserControler.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserControler.java
@@ -3,15 +3,22 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.exceptions.UserExistsException;
+import com.stacksimplify.restservices.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.services.UserService;
 
 @RestController
@@ -22,19 +29,34 @@ public class UserControler {
 	@GetMapping("/users")
 	public List<User> getAllUsers(){
 		return userService.getAllUsers();
-	}
-	@PostMapping("/users")
+	} 
 	
-	public User createUser(@RequestBody User user) {
-		return userService.createUser(user);
+	@PostMapping("/users")
+	public ResponseEntity<Void> createUser(@RequestBody User user,UriComponentsBuilder builder) {
+		try {
+		 userService.createUser(user);
+		 HttpHeaders headers = new HttpHeaders();
+		 headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+		 return new ResponseEntity<Void>(headers,HttpStatus.CREATED);
+		}catch(UserExistsException ex) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+		}
 	}
 	@GetMapping("/users/{id}")
 	public Optional<User> getUserById(@PathVariable("id") Long id){
-		return userService.getUserById(id);
+		try {
+			return userService.getUserById(id);
+		}catch(UserNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+		}
 	}
 	@PutMapping("/users/{id}")
 	public User updateUserById(@PathVariable("id")Long id, @RequestBody User user) {
-		return userService.updateUserById(id, user);
+		try {
+			return userService.updateUserById(id, user);	
+	}catch(UserNotFoundException ex){
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+		}
 	}
 	@DeleteMapping("/users/{id}")
 	public void deleteUserById(@PathVariable("id")Long id) {
---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserExistsException.java
@@ -0,0 +1,15 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1904585489531578456L;
+
+	public UserExistsException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub 
+	}
+	
+}
