210203a748458512e977a64fe2afdf8a6767c050
Bohdan Stelmakh <bohdan.stelmakh@softiero.com>
2020-08-13 18:46:45 +0300

    JsonViews - First Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -16,38 +16,47 @@ import org.springframework.hateoas.ResourceSupport;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
 
 
 @Entity
 @Table(name = "user", schema="usermanagement")
 //@JsonIgnoreProperties({"firstname","lastname"})
-@JsonFilter(value="userFilter")
+//@JsonFilter(value="userFilter")
 public class User extends ResourceSupport {  
 	
 	@Id
 	@GeneratedValue
+	@JsonView(Views.External.class)
 	private Long userid; 
 	
 	@NotEmpty(message="Username is Mark field,Please provide username")
 	@Column(name="USER_NAME",length=50,nullable=false,unique=true)
+	@JsonView(Views.External.class)
 	private String username;
 	
 	@Size(min=2 ,message="FirstName should have atleast 2 characters")
 	@Column(name="FIRST_NAME",length=50,nullable=false)
+	@JsonView(Views.External.class)
 	private String firstname;
 	
 	@Column(name="LAST_NAME",length=50,nullable=false)
+	@JsonView(Views.External.class)
 	private String lastname;
 	@Column(name="EMAIL_ADDRESS",length=50,nullable=false)
+	@JsonView(Views.External.class)
 	private String Email;
 	@Column(name="ROLE",length=50,nullable=false)
+	@JsonView(Views.Internal.class)
 	private String role;
 	
 	@Column(name="SSN",length=50,nullable=false,unique=true)
+	@JsonView(Views.Internal.class)
 //	@JsonIgnore
 	private String ssn;
 	
 	@OneToMany(mappedBy="user")
+	@JsonView(Views.Internal.class)
 	private List<Order> orders;	
 	
 	public User() {	
